ee2d53763111b619e4bbc5584544399f1e261295
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2016-05-14 19:06:58 -0400

    listas T0101,T0102,T0106,T0107,T0201,T0203,T0204,T1001,T1002

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,144 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/settings.html',
+    controller: 'tasteNotecontroller'
+  })
+
+
+    .state('app.menu', {
+      url: '/menu',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/menu.html'
+        }
+      }
+    })
+    .state('app.discover', {
+      url: "/discover",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/discover.html"
+        }
+      }
+    })
+    .state('app.info', {
+      url: '/info',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/info.html'
+        }
+      }
+    })
+        .state('app.color', {
+      url: '/color',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/color.html'
+        }
+      }
+    })
+        .state('app.smell', {
+      url: '/smell',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/smell.html'
+        }
+      }
+    })
+
+    .state('app.taste', {
+      url: '/taste',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/taste.html'
+        }
+      }
+    })
+     .state('app.review', {
+      url: '/review',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/review.html'
+        }
+      }
+    })
+     .state('app.mynotes', {
+      url: '/notes',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/notes.html',
+          controller: 'tasteNotecontroller'
+        }
+      }
+    })
+    .state('app.discovernotes', {
+     url: '/discovernotes',
+     views: {
+       'menuContent': {
+         templateUrl: 'templates/discovernotes.html'
+       }
+     }
+   })
+   .state('app.help-info', {
+    url: '/help-info',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/help-info.html'
+      }
+    }
+  })
+  .state('app.help-smell', {
+   url: '/help-smell',
+   views: {
+     'menuContent': {
+       templateUrl: 'templates/help-smell.html'
+     }
+   }
+ })
+ .state('app.help-taste', {
+  url: '/help-taste',
+  views: {
+    'menuContent': {
+      templateUrl: 'templates/help-taste.html'
+    }
+  }
+})
+.state('app.help-color', {
+ url: '/help-color',
+ views: {
+   'menuContent': {
+     templateUrl: 'templates/help-color.html'
+   }
+ }
+})
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/menu');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,196 @@
+'use strict';
+var  ctrl=angular.module('starter.controllers', ['ionic'])
+
+ctrl.service('dataService', function($http) {
+this.getColor = function(callbackFunc) {
+  var color = 'color';
+
+    $http({
+
+        method: 'GET',
+        url: 'https://rum-backend-db.herokuapp.com/'+color
+     }).success(function(data){
+        // With the data succesfully returned, call our callback
+        console.log(data);
+        callbackFunc(data);
+    }).error(function(){
+        alert("error");
+    });
+ }
+ this.getSmell = function(callbackFunc) {
+     $http({
+         method: 'GET',
+         url: 'https://rum-backend-db.herokuapp.com/smell'
+      }).success(function(data){
+         // With the data succesfully returned, call our callback
+         callbackFunc(data);
+     }).error(function(){
+         alert("error");
+     });
+  }
+  this.getNotes = function(callbackFunc) {
+      $http({
+          method: 'GET',
+          url: 'https://rum-backend-db.herokuapp.com/note'
+       }).success(function(data){
+          // With the data succesfully returned, call our callback
+          callbackFunc(data);
+      }).error(function(){
+          alert("error");
+      });
+   }
+  this.getTaste = function(callbackFunc) {
+      $http({
+          method: 'GET',
+          url: 'https://rum-backend-db.herokuapp.com/taste'
+       }).success(function(data){
+          // With the data succesfully returned, call our callback
+          callbackFunc(data);
+      }).error(function(){
+          alert("error");
+      });
+   }
+   this.getRum = function(callbackFunc) {
+       $http({
+           method: 'GET',
+           url: 'https://rum-backend-db.herokuapp.com/rum'
+        }).success(function(data){
+           // With the data succesfully returned, call our callback
+           callbackFunc(data);
+           console.log(data);
+
+       }).error(function(){
+           alert("error");
+       });
+    }
+});
+
+ctrl.controller('tasteNotecontroller', function($scope, dataService, $http) {
+  $scope.search={};
+  $scope.choice='';
+  $scope.choice2='';
+  $scope.choice3='';
+  $scope.refresh
+$scope.note={brand: ''};
+    $scope.color = dataService.getColor(function(dataResponse) {
+       $scope.color = dataResponse;
+
+    });
+    $scope.smell = dataService.getSmell(function(dataResponse) {
+      $scope.checked = {word: ''}
+      $scope.itensitySmel= 3;
+      $scope.smell = dataResponse;
+      $scope.smells=$scope.pudge($scope.smell,$scope.checked.word);
+      $scope.refresh=function(){
+        $scope.smells=$scope.pudge($scope.smell,$scope.checked.word);
+      }
+
+
+    });
+    $scope.taste = dataService.getTaste(function(dataResponse) {
+      $scope.checked2 = {word: ''}
+      $scope.itensityTaste= 3;
+      $scope.taste = dataResponse;
+      $scope.tastes=$scope.pudge($scope.taste,$scope.checked2.word);
+      $scope.refresh2=function(){
+        $scope.tastes=$scope.pudge($scope.taste,$scope.checked2.word);
+      }
+
+
+    });
+    $scope.rum = dataService.getRum(function(dataResponse) {
+      $scope.rum = dataResponse;
+      $scope.brand=$scope.nonrepeatbrand($scope.rum);
+      $scope.name=$scope.relatedto($scope.rum);
+
+    });
+    $scope.notes = dataService.getNotes(function(dataResponse) {
+      $scope.notes = dataResponse;
+    });
+
+    $scope.relatedto=function(word,rum){
+      var array=[];
+      for (var i = 0; i < rum.length; i++) {
+        if(rum[i].brand==word){
+          array.push(rum[i]);
+
+        }
+      }
+      return array;
+    }
+    $scope.nonrepeatbrand=function(rum) {
+      var array2=[];
+      for (var i = 0; i < rum.length; i++) {
+        if(!$scope.repeatword(rum[i].brand,array2)){
+            array2.push(rum[i]);
+      }
+
+    }
+      return array2;
+    }
+    $scope.repeatword=function(word,array) {
+      for (var j = 0; j < array.length; j++) {
+        if(array[j].brand==word){
+          return true;
+        }
+      }
+      return false;
+    }
+    $scope.pudge=function(smell,word){
+    var array =[];
+      for (var i = 0; i <smell.length; i++) {
+        if(smell[i].name==word || smell[i].category==word){
+          if(smell[i].name==word){
+            for (var j = 0; j < smell.length; j++) {
+              if(smell[j].category==smell[i].category){
+                array.push(smell[j]);
+              }
+            }
+          }else{
+          array.push(smell[i]);
+          }
+        }
+
+      }
+      return array;
+    };
+
+    $scope.getID=function (word,rum) {
+      var id = 0;
+      for (var i = 0; i < rum.length; i++) {
+        if(rum[i].name==word){
+          id = rum[i].id;
+        }
+      }
+      return id;
+    }
+
+    $scope.postNote=function (rum,taste,smell,color){
+     var data ={
+        user_id: '1'
+        , rum_id: rum,
+         taste_id: taste ,
+        tastedIntensity: '3',
+         smell_id: smell,
+          smellIntensity: '3',
+           color_id: color
+
+     };
+
+
+
+            $http.post('https://rum-backend-db.herokuapp.com/note', data)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+
+
+    };
+
+});
